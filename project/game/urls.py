fd9c325ac4cf87f79262647323851382de405894
hamez611 <hamez611@naver.com>
2024-06-08 15:03:48 +0900

    Feat: 교수+학과 모델 추가

--- project/game/urls.py
+++ project/game/urls.py
@@ -1,9 +1,14 @@
 # myapp/urls.py
+from django.contrib import admin
 from django.urls import path
-from . import views
+from game import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('start/', views.start, name='start'),
-    path('add_users/', views.add_user, name = 'add_user'),
-]
+    path('add_users/', views.add_user, name='add_user'),
+    path('game/', views.game, name='game'),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/game/views.py
+++ project/game/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
 
 # Create your views here.
+
+
 def main(request):
     return render(request, 'main.html')
 
+
 def start(request):
     return render(request, 'start.html')
+
 
 def add_user(request):
     if request.method == 'POST':
@@ -19,9 +23,21 @@ def add_user(request):
             return redirect('user_list')
     else:
         form = UserForm()
-        
+
     ctx = {
         'form': form,
     }
-    
+
     return render(request, 'game.html', ctx)
+
+
+def main(request):
+    departments = Department.objects.all()
+    professors = Professor.objects.all()
+    return render(request, 'main.html', {'departments': departments, 'professors': professors})
+
+
+def game(request):
+    professor_id = request.GET.get('professor')
+    professor = get_object_or_404(Professor, id=professor_id)
+    return render(request, 'game.html', {'professor': professor})
